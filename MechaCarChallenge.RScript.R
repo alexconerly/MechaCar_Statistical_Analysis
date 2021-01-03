4f377cf92cbcb58a9ebfa27396bd29e861190d46
alexconerly <alexanderconerly@gmail.com>
2021-01-03 13:16:53 -0600

    r script update

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,39 +1,42 @@
 
 #Module 15 Challenge, 
 
-#Deliverable 1:  Linear Regression to Predict MPG
+#DELIVERABLE 1:  Linear Regression to Predict MPG
 
 library(tidyr)
 df <- read.csv("MechaCar_mpg.csv",check.names=F,stringsAsFactors = F)
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
+mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
                 ground_clearance + AWD, data=df)
 
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
-             ground_clearance + AWD, data=df))
+summary(mecha_lm)
 
-#Deliverable 2:  Create Visualizations for the Trip Analysis
+#DELIVERABLE 2:  Create Visualizations for the Trip Analysis
 
 df2 <- read.table("Suspension_Coil.csv", header=T, sep = ",", check.names=F,
                   stringsAsFactors = F)
 
-total_summary <- summarize(df2, Mean=mean(PSI),
-                                    Median=median(PSI),
-                                    Variance=var(PSI),
-                                    SD=sd(PSI))
+total_summary <- df2 %>% summarize(Mean=mean(PSI),
+                                   Median=median(PSI),
+                                   Variance=var(PSI),
+                                   SD=sd(PSI))
 lot_summary <- df2 %>% 
-              group_by(Manufacturing_Lot) %>%
-              summarize(Mean=mean(PSI),
-                           Median=median(PSI),
-                           Variance=var(PSI),
-                           SD=sd(PSI))
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI),
+            Median=median(PSI),
+            Variance=var(PSI),
+            SD=sd(PSI),
+            .groups='keep')
 
-#Deliverable 3: T-Tests on Suspension Coils
+#DELIVERABLE 3: T-Tests on Suspension Coils
 
+# Perform t-test across all Lots
 t.test(df2$PSI, mu=1500)
 
-t.test(subset(df2, PSI == "Lot1"), mu=1500)
+#Perform t-test on lot1
+t.test(subset(df2,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+#Perform t-test on lot2
+t.test(subset(df2,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+#Perform t-test on lot3
+t.test(subset(df2,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
 
-t.test(formula = 1500 ~ PSI,
-       data = df2,
-       subset = df2$PSI == "Lot1")
